52f7fae4491cefcebcff95d98101ae2de7d41d2e
Guilherme Garcia <garciaguig@gmail.com>
2025-09-30 09:19:42 -0300

    feat: implement theme provider from 'theme context' on root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,7 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider
-} from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
-import { useColorScheme } from 'react-native'
+
+import { ThemeProvider } from '@/contexts/theme-context'
 
 import 'react-native-reanimated'
 
@@ -14,10 +10,8 @@ export const unstable_settings = {
 }
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen

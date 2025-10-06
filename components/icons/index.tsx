976af74a71f418a765c1056fcaaf8f43ac000edd
Guilherme Garcia <garciaguig@gmail.com>
2025-10-06 20:27:21 -0300

    feat: add new icons components

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -1,6 +1,23 @@
 import Feather from '@expo/vector-icons/Feather'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 
 import { IconProps } from '@/types/ui'
+
+export function EmailIcon({
+  size = 24,
+  color = 'white',
+  className
+}: IconProps) {
+  return (
+    <MaterialIcons
+      name="alternate-email"
+      size={size}
+      color={color}
+      className={className}
+    />
+  )
+}
 
 export function EyeIcon({ size = 24, color = 'white', className }: IconProps) {
   return <Feather name="eye" size={size} color={color} className={className} />
@@ -13,5 +30,11 @@ export function EyeOffIcon({
 }: IconProps) {
   return (
     <Feather name="eye-off" size={size} color={color} className={className} />
+  )
+}
+
+export function LockIcon({ size = 24, color = 'white', className }: IconProps) {
+  return (
+    <FontAwesome name="lock" size={size} color={color} className={className} />
   )
 }

c9ca3cba54d1ca19c2c66c63bba338f9def5deac
Guilherme Garcia <garciaguig@gmail.com>
2025-10-08 20:35:50 -0300

    feat: create 'auth' service with 'login' method

---
+++ services/auth.ts
@@ -0,0 +1,9 @@
+import api from '@/services/api'
+import { LoginRequest, LoginResponse } from '@/types/auth'
+
+export const login = async (
+  credentials: LoginRequest
+): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>('/auth/login', credentials)
+  return response.data
+}

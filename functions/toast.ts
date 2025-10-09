48793f8e595d4eac16a5ecdce5c7850075ea7595
Guilherme Garcia <garciaguig@gmail.com>
2025-10-08 21:42:15 -0300

    feat: implement show toast functions

---
+++ functions/toast.ts
@@ -0,0 +1,17 @@
+import Toast from 'react-native-toast-message'
+
+export const showSuccessToast = (title: string, description?: string) => {
+  Toast.show({
+    type: 'success',
+    text1: title,
+    text2: description
+  })
+}
+
+export const showErrorToast = (title: string, description?: string) => {
+  Toast.show({
+    type: 'error',
+    text1: title,
+    text2: description
+  })
+}

fa34e59a71910328fbf4ae5c69722702dee11612
Guilherme Garcia <garciaguig@gmail.com>
2025-10-08 21:41:59 -0300

    feat: implement 'react native toast message' with custom toast config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import Toast from 'react-native-toast-message'
 
+import { CUSTOM_TOAST_CONFIG } from '@/components/custom-toast'
 import { ThemeProvider } from '@/contexts/theme-context'
-
 import 'react-native-reanimated'
 
 export const unstable_settings = {
@@ -17,6 +18,13 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
+      <Toast
+        autoHide={true}
+        position="top"
+        topOffset={60}
+        visibilityTime={3000}
+        config={CUSTOM_TOAST_CONFIG}
+      />
     </ThemeProvider>
   )
 }

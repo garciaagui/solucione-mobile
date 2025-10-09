4ec36d7bcd9ab084fce97c2f138dd5db9476d27c
Guilherme Garcia <garciaguig@gmail.com>
2025-10-09 18:44:15 -0300

    feat: implement 'QueryClientProvider' and 'AuthProvider' on root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import Toast from 'react-native-toast-message'
 
 import { CUSTOM_TOAST_CONFIG } from '@/components/custom-toast'
+import { AuthProvider } from '@/contexts/auth-context'
 import { ThemeProvider } from '@/contexts/theme-context'
 import 'react-native-reanimated'
 
@@ -11,20 +13,26 @@ export const unstable_settings = {
 }
 
 export default function RootLayout() {
+  const queryClient = new QueryClient()
+
   return (
-    <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-      <Toast
-        autoHide={true}
-        position="top"
-        topOffset={60}
-        visibilityTime={3000}
-        config={CUSTOM_TOAST_CONFIG}
-      />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <Stack>
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="auto" />
+          <Toast
+            autoHide={true}
+            position="top"
+            topOffset={60}
+            visibilityTime={3000}
+            config={CUSTOM_TOAST_CONFIG}
+          />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }

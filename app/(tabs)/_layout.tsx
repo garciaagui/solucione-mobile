f5205c4c220f73438142ff7479959f462410a7ff
Guilherme Garcia <garciaguig@gmail.com>
2025-10-10 20:54:48 -0300

    feat: implement new structure for tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,49 +1,18 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { useColorScheme } from 'react-native'
 
-import { HapticTab } from '@/components/haptic-tab'
-import { IconSymbol } from '@/components/ui/icon-symbol'
-import { Colors } from '@/constants/theme'
+import TabBar from '@/components/tab-bar'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab
-      }}>
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Perfil',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          )
-        }}
-      />
-
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="complaints"
-        options={{
-          title: 'Reclamações',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          )
-        }}
-      />
+      screenOptions={{ headerShown: false }}
+      backBehavior="history"
+      initialRouteName="index"
+      tabBar={props => <TabBar {...props} />}>
+      <Tabs.Screen name="profile" />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="complaints" />
     </Tabs>
   )
 }

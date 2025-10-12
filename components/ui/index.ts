28a136ad29d7798d3b50ebeee999d241ee7f42fa
Guilherme Garcia <garciaguig@gmail.com>
2025-10-12 15:06:59 -0300

    feat: create 'skeleton' ui component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,7 @@
 import Button from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import Skeleton from '@/components/ui/skeleton'
 import Text from '@/components/ui/text'
 import ThemedView from '@/components/ui/themed-view'
 
-export { Button, Input, Text, ThemedView }
+export { Button, Input, Skeleton, Text, ThemedView }

99d7ecab768cafe1b76c589f0f70cc0e979f0ce8
Guilherme Garcia <garciaguig@gmail.com>
2025-10-12 14:37:21 -0300

    feat: implement new structure for 'home' screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,67 +1,38 @@
-import { Image } from 'expo-image'
-import { Link } from 'expo-router'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { useMemo } from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import ParallaxScrollView from '@/components/parallax-scroll-view'
-import { Button, Text, ThemedView } from '@/components/ui'
-import { useAuth } from '@/contexts/auth-context'
-import { showErrorToast, showSuccessToast } from '@/functions/toast'
+import { HomeHeader } from '@/components/app/home'
+import { useTheme } from '@/contexts/theme-context'
+import { ThemeColors } from '@/types/ui'
 
 export default function HomeScreen() {
-  const { user, logout } = useAuth()
+  const { colors } = useTheme()
+  const styles = useMemo(() => createStyles(colors), [colors])
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.container}>
-        <Text size="3xl" weight="bold">
-          Welcome!
-        </Text>
-
-        <View>
-          <Text weight="semibold">User: {user?.name || 'Não logado'}</Text>
-
-          <Link href="/sign-in" asChild>
-            <Text weight="semibold">Sign in</Text>
-          </Link>
-
-          <TouchableOpacity onPress={() => logout()}>
-            <Text weight="semibold">Logout</Text>
-          </TouchableOpacity>
-        </View>
-
-        <Button
-          onPress={() =>
-            showSuccessToast('Success', 'This is a success toast')
-          }>
-          Teste toast de sucesso
-        </Button>
-        <Button
-          onPress={() => showErrorToast('Error', 'This is a error toast')}>
-          Teste toast de erro
-        </Button>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+        style={styles.container}>
+        <HomeHeader />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    gap: 8
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute'
-  }
-})
+const createStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: colors.background,
+      flex: 1,
+      paddingHorizontal: 24
+    },
+    header: {
+      gap: 16,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    }
+  })

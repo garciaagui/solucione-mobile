66fba6fbfb76bc023424323708a86c7d74c53b35
Guilherme Garcia <garciaguig@gmail.com>
2025-10-12 22:27:43 -0300

    feat: implement 'complaints container' component on 'home' screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,16 @@ import { useMemo } from 'react'
 import { ScrollView, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import ComplaintsContainer from '@/components/app/complaints-container'
 import { HomeHeader } from '@/components/app/home'
 import { useTheme } from '@/contexts/theme-context'
+import { useComplaints } from '@/hooks/use-complaints'
 import { ThemeColors } from '@/types/ui'
 
 export default function HomeScreen() {
   const { colors } = useTheme()
   const styles = useMemo(() => createStyles(colors), [colors])
+  const { data, error, isLoading } = useComplaints()
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -17,6 +20,7 @@ export default function HomeScreen() {
         showsVerticalScrollIndicator={false}
         style={styles.container}>
         <HomeHeader />
+        <ComplaintsContainer isLoading={isLoading} complaints={data} />
       </ScrollView>
     </SafeAreaView>
   )

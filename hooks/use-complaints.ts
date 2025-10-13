61c1460d57a71c7a2e3747516097fd45d87450a4
Guilherme Garcia <garciaguig@gmail.com>
2025-10-13 18:57:37 -0300

    feat: implement refetch logic on 'useComplaints' hook

--- hooks/use-complaints.ts
+++ hooks/use-complaints.ts
@@ -1,14 +1,30 @@
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 
 import { COMPLAINTS_QUERY_KEY } from '@/constants/query-keys'
 import { getComplaints } from '@/services/complaint'
 import { Complaint } from '@/types/complaint'
 
 export const useComplaints = () => {
-  const { data, error, isLoading } = useQuery<Complaint[]>({
+  const [isRefreshing, setIsRefreshing] = useState(false)
+
+  const {
+    data,
+    error,
+    isLoading: isLoadingQuery,
+    refetch: refetchQuery
+  } = useQuery<Complaint[]>({
     queryKey: [COMPLAINTS_QUERY_KEY],
     queryFn: getComplaints
   })
 
-  return { data, error, isLoading }
+  const refetch = async () => {
+    setIsRefreshing(true)
+    await refetchQuery()
+    setIsRefreshing(false)
+  }
+
+  const isLoading = isLoadingQuery || isRefreshing
+
+  return { data, error, isLoading, refetch }
 }

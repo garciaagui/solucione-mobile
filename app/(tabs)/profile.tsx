50ef0eb8d3c3a53d1e6a11648bd05343bfb2cf9c
Guilherme Garcia <garciaguig@gmail.com>
2025-10-20 18:08:57 -0300

    feat: implement components on profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,23 +2,44 @@ import { useMemo } from 'react'
 import { ScrollView, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { Text } from '@/components/ui'
+import {
+  ActionsContainer,
+  AvatarContainer,
+  InfoContainer
+} from '@/components/app/profile'
+import UnauthenticatedContainer from '@/components/app/unauthenticated-container'
+import { useAuth } from '@/contexts/auth-context'
 import { useTheme } from '@/contexts/theme-context'
 import { ThemeColors } from '@/types/ui'
 
 export default function ProfileScreen() {
+  const { user } = useAuth()
+
   const { colors } = useTheme()
   const styles = useMemo(() => createStyles(colors), [colors])
+
+  const renderContent = () => {
+    if (!user) {
+      return <UnauthenticatedContainer />
+    }
+
+    return (
+      <>
+        <AvatarContainer user={user} />
+        <InfoContainer user={user} />
+        <ActionsContainer />
+      </>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
-        style={styles.container}>
-        <Text size="2xl" weight="bold" variant="primary">
-          Meu perfil
-        </Text>
+        style={styles.container}
+        contentContainerStyle={{ gap: 24 }}>
+        {renderContent()}
       </ScrollView>
     </SafeAreaView>
   )
@@ -33,6 +54,7 @@ const createStyles = (colors: ThemeColors) =>
     container: {
       backgroundColor: colors.background,
       flex: 1,
-      paddingHorizontal: 16
+      paddingHorizontal: 16,
+      paddingTop: 24
     }
   })

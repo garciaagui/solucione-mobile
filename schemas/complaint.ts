8d6f66770234df3d96091c743bd87e788b496d96
Guilherme Garcia <garciaguig@gmail.com>
2025-10-20 19:42:20 -0300

    feat: create complaint schemas

---
+++ schemas/complaint.ts
@@ -0,0 +1,64 @@
+import { z } from 'zod'
+
+const firstStep = z.object({
+  title: z
+    .string({
+      required_error: 'O título é obrigatório'
+    })
+    .min(10, {
+      message: 'O título precisa ter no mínimo 10 caracteres'
+    })
+    .max(127, {
+      message: 'O título pode ter no máximo 127 caracteres'
+    }),
+  description: z
+    .string({
+      required_error: 'A descrição é obrigatória'
+    })
+    .min(40, {
+      message: 'A descrição precisa ter no mínimo 40 caracteres'
+    })
+    .max(255, {
+      message: 'A descrição pode ter no máximo 255 caracteres'
+    })
+})
+
+const secondStep = z.object({
+  neighborhood: z
+    .string({
+      required_error: 'O bairro é obrigatório'
+    })
+    .min(1, {
+      message: 'Insira o bairro'
+    }),
+  zipCode: z
+    .string({
+      required_error: 'O CEP é obrigatório'
+    })
+    .length(10, {
+      message: 'CEP precisa ter exatamente 8 dígitos'
+    }),
+  // Embora o necessário seja 8 dígitos, consideramos 10 por conta do ponto e traço. Exemplo: 18.540-000.
+  street: z
+    .string({
+      required_error: 'A rua é obrigatória'
+    })
+    .min(1, {
+      message: 'Insira a rua'
+    }),
+  reference: z.string().optional()
+})
+
+const thirdStep = z.object({
+  image: z.custom<File>(file => file instanceof File, {
+    message: 'A imagem é obrigatória'
+  })
+})
+
+export const newComplaintSchema = z.object({
+  firstStep,
+  secondStep,
+  thirdStep
+})
+
+export type NewComplaintFormValues = z.infer<typeof newComplaintSchema>

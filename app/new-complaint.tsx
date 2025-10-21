86671e0417fda310253da774792cc7024eb31f0b
Guilherme Garcia <garciaguig@gmail.com>
2025-10-21 11:25:18 -0300

    feat: implement initial step logic for new complaint modal

--- app/new-complaint.tsx
+++ app/new-complaint.tsx
@@ -1,5 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'expo-router'
 import { useMemo } from 'react'
+import { useForm } from 'react-hook-form'
 import {
   KeyboardAvoidingView,
   Platform,
@@ -9,19 +11,53 @@ import {
   View
 } from 'react-native'
 
+import { FirstFormStep } from '@/components/app/_screens/new-complaint'
 import { XIcon } from '@/components/icons'
 import { Text } from '@/components/ui'
+import { StepProvider, useStep } from '@/contexts/step-context'
 import { useTheme } from '@/contexts/theme-context'
+import { NewComplaintFormValues, newComplaintSchema } from '@/schemas/complaint'
 import { ThemeColors } from '@/types/ui'
 
-export default function CreateComplaintModal() {
+function ModalContent() {
   const router = useRouter()
+  const { currentStep } = useStep()
+
+  const form = useForm<NewComplaintFormValues>({
+    resolver: zodResolver(newComplaintSchema),
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      firstStep: {
+        title: '',
+        description: ''
+      },
+      secondStep: {
+        neighborhood: '',
+        reference: '',
+        street: '',
+        zipCode: ''
+      },
+      thirdStep: {
+        image: undefined
+      }
+    }
+  })
 
   const { colors } = useTheme()
   const styles = useMemo(() => createStyles(colors), [colors])
 
   const handleClose = () => {
     router.back()
+  }
+
+  const renderFormStep = () => {
+    switch (currentStep) {
+      case 1:
+        return <FirstFormStep form={form} />
+      default:
+        return null
+    }
   }
 
   return (
@@ -40,8 +76,22 @@ export default function CreateComplaintModal() {
       <ScrollView
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
-        style={styles.scrollView}></ScrollView>
+        contentContainerStyle={styles.scrollViewContent}
+        style={styles.scrollView}>
+        <Text size="sm" variant="secondary" style={styles.stepIndicator}>
+          Passo {currentStep} de 3
+        </Text>
+        {renderFormStep()}
+      </ScrollView>
     </KeyboardAvoidingView>
+  )
+}
+
+export default function NewComplaintModal() {
+  return (
+    <StepProvider initialStep={1} stepsNumber={3}>
+      <ModalContent />
+    </StepProvider>
   )
 }
 
@@ -63,6 +113,13 @@ const createStyles = (colors: ThemeColors) =>
       marginBottom: 24
     },
     scrollView: {
-      flex: 1
+      flex: 1,
+      paddingBottom: 48
+    },
+    scrollViewContent: {
+      flexGrow: 1
+    },
+    stepIndicator: {
+      marginBottom: 16
     }
   })

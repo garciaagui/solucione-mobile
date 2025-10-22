b186a23d662e734b32892582ef4493f2fa4cc66b
Guilherme Garcia <garciaguig@gmail.com>
2025-10-22 10:58:19 -0300

    feat: implement new form structure on new complaint modal

--- app/new-complaint.tsx
+++ app/new-complaint.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'expo-router'
 import { useMemo } from 'react'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import {
   KeyboardAvoidingView,
   Platform,
@@ -9,55 +9,41 @@ import {
   StyleSheet
 } from 'react-native'
 
-import {
-  FirstFormStep,
-  NewComplaintHeader
-} from '@/components/app/_screens/new-complaint'
-import { Text } from '@/components/ui'
-import { StepProvider, useStep } from '@/contexts/step-context'
+import { NewComplaintHeader } from '@/components/app/_screens/new-complaint'
+import { Button, Input, Textarea } from '@/components/ui'
 import { useTheme } from '@/contexts/theme-context'
+import { formatZipCode } from '@/functions/complaints'
 import { NewComplaintFormValues, newComplaintSchema } from '@/schemas/complaint'
 import { ThemeColors } from '@/types/ui'
 
-function ModalContent() {
+export default function NewComplaintModal() {
   const router = useRouter()
-  const { currentStep } = useStep()
-
-  const form = useForm<NewComplaintFormValues>({
-    resolver: zodResolver(newComplaintSchema),
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    defaultValues: {
-      firstStep: {
-        title: '',
-        description: ''
-      },
-      secondStep: {
-        neighborhood: '',
-        reference: '',
-        street: '',
-        zipCode: ''
-      },
-      thirdStep: {
-        image: undefined
-      }
-    }
-  })
 
   const { colors } = useTheme()
   const styles = useMemo(() => createStyles(colors), [colors])
+
+  const form = useForm<NewComplaintFormValues>({
+    resolver: zodResolver(newComplaintSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      neighborhood: '',
+      reference: '',
+      street: '',
+      zipCode: '',
+      image: undefined
+    }
+  })
+
+  const { control, formState, handleSubmit } = form
+  const errors = formState.errors
 
   const handleClose = () => {
     router.back()
   }
 
-  const renderFormStep = () => {
-    switch (currentStep) {
-      case 1:
-        return <FirstFormStep form={form} />
-      default:
-        return null
-    }
+  const onSubmit = (data: NewComplaintFormValues) => {
+    console.log(data)
   }
 
   return (
@@ -71,28 +57,101 @@ function ModalContent() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.scrollViewContent}
         style={styles.scrollView}>
-        <Text size="sm" variant="secondary" style={styles.stepIndicator}>
-          Passo {currentStep} de 3
-        </Text>
-        {renderFormStep()}
+        <Controller
+          control={control}
+          name="title"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label="Título"
+              variant="text"
+              errorMessage={errors.title?.message}
+              autoCapitalize="none"
+              placeholder="Ex: Falta de coleta de lixo"
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange, value } }) => (
+            <Textarea
+              label="Descrição"
+              errorMessage={errors.description?.message}
+              placeholder="Descreva detalhadamente o problema ocorrido, mencionando horários, frequência e impacto."
+              maxLength={255}
+              minHeight={120}
+              showCharCount={true}
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="street"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label="Rua"
+              variant="text"
+              errorMessage={errors.street?.message}
+              autoCapitalize="none"
+              placeholder="Ex: Rua das Flores, n. 123"
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="neighborhood"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label="Bairro"
+              variant="text"
+              errorMessage={errors.neighborhood?.message}
+              autoCapitalize="none"
+              placeholder="Ex: Centro"
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="zipCode"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              label="CEP"
+              variant="text"
+              errorMessage={errors.zipCode?.message}
+              autoCapitalize="none"
+              placeholder="Ex: 12.345-000"
+              maxLength={10}
+              value={value}
+              onChangeText={e => {
+                const formatted = formatZipCode(e)
+                onChange(formatted)
+              }}
+            />
+          )}
+        />
+
+        <Button onPress={handleSubmit(onSubmit)} style={styles.button}>
+          Enviar
+        </Button>
       </ScrollView>
     </KeyboardAvoidingView>
   )
 }
 
-export default function NewComplaintModal() {
-  return (
-    <StepProvider initialStep={1} stepsNumber={3}>
-      <ModalContent />
-    </StepProvider>
-  )
-}
-
 const createStyles = (colors: ThemeColors) =>
   StyleSheet.create({
-    closeButton: {
-      padding: 4
-    },
     container: {
       backgroundColor: colors.background,
       flex: 1,
@@ -104,9 +163,10 @@ const createStyles = (colors: ThemeColors) =>
       paddingBottom: 48
     },
     scrollViewContent: {
-      flexGrow: 1
+      flexGrow: 1,
+      gap: 12
     },
-    stepIndicator: {
-      marginBottom: 16
+    button: {
+      marginTop: 12
     }
   })

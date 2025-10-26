eee2d97a17287fc58aa5c681ad5e06a9f5349c08
Guilherme Garcia <garciaguig@gmail.com>
2025-10-26 10:55:39 -0300

    feat: create 'createComplaint' complaint service method

--- services/complaint.ts
+++ services/complaint.ts
@@ -6,3 +6,8 @@ export const getComplaints = async (): Promise<Complaint[]> => {
   const response = await api.get<Complaint[]>('/complaints')
   return response.data
 }
+
+export const createComplaint = async (data: FormData): Promise<any> => {
+  const response = await api.post<any>('/complaints', data)
+  return response.data
+}

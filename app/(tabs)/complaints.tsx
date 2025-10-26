742d9bb0e05abfa11d00c554f2d7cca1a6a09272
Guilherme Garcia <garciaguig@gmail.com>
2025-10-26 16:48:14 -0300

    feat: implement unauthenticated container component on complaints screen

--- app/(tabs)/complaints.tsx
+++ app/(tabs)/complaints.tsx
@@ -1,14 +1,36 @@
 import { useMemo } from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
+import UnauthenticatedContainer from '@/components/app/unauthenticated-container'
 import { Text } from '@/components/ui'
+import { useAuth } from '@/contexts/auth-context'
 import { useTheme } from '@/contexts/theme-context'
 import { ThemeColors } from '@/types/ui'
 
 export default function ComplaintsScreen() {
+  const { user } = useAuth()
+
   const { colors } = useTheme()
   const styles = useMemo(() => createStyles(colors), [colors])
+
+  const renderContent = () => {
+    if (!user) {
+      return (
+        <View style={{ paddingTop: 24 }}>
+          <UnauthenticatedContainer description="Faça login para acessar suas reclamações." />
+        </View>
+      )
+    }
+
+    return (
+      <>
+        <Text size="xl" weight="bold" variant="primary">
+          Reclamações
+        </Text>
+      </>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -16,9 +38,7 @@ export default function ComplaintsScreen() {
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={false}
         style={styles.container}>
-        <Text size="xl" weight="bold" variant="primary">
-          Reclamações
-        </Text>
+        {renderContent()}
       </ScrollView>
     </SafeAreaView>
   )
